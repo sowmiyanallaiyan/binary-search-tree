8fcf9a51e6c35723ca5195d2d925a9c43bb98527
saumya-2259 <91017822+saumya-2259@users.noreply.github.com>
2021-11-01 19:28:09 +0530

    deletion function is added

--- tree.c
+++ tree.c
@@ -25,6 +25,47 @@ else
 T->right=insert(x,T->right);
 return T;
 }
+struct node *delete1(struct node *T,int x)
+{
+    if(T==NULL)
+    return NULL;
+    if(x<T->data)
+    T->left=delete1(T->left,x);
+    else if(x>T->data)
+    T->right=delete1(T->right,x);
+    else
+    {
+        if((T->left==NULL)&&(T->right==NULL))
+        return NULL;
+         if((T->left==NULL)&&(T->right!=NULL))
+        return (T->right);
+         if((T->left!=NULL)&&(T->right==NULL))
+        return (T->left);
+        else
+        {
+            struct node *q;
+            q=T->right;
+            while(q->left!=NULL)
+            {
+                q=q->left;
+                T->data=q->data;
+                T->right=delete1(T->right,q->data);
+            }
+        }
+    }
+    return T;
+}
+void inorder(struct node *t1)
+{
+if(t1==NULL)
+return;
+else
+{
+inorder(t1->left);
+printf("\t%d",t1->data);
+inorder(t1->right);
+}
+}
 void main()
 {
 struct node *root=NULL;
@@ -34,6 +75,9 @@ root=insert (40,root);
 root=insert(100,root);
 root=insert(99,root);
 root=insert(5,root);
+    delete1(root,52);
+printf("\nbinary search tree after deletion\n");
+inorder(root);
 }
 
     
